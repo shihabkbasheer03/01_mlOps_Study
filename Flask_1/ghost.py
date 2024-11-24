fb262b452ab2ad2917f25d2e35c564199c4e8263
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-25 04:59:03 +0530

    updated

--- Flask_1/ghost.py
+++ Flask_1/ghost.py
@@ -14,14 +14,19 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route("/form", methods=['GET'])
+@app.route("/form", methods=['GET','POST'])
 def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
     return render_template('form.html')
 
-@app.route("/submit", methods=['POST'])
+@app.route("/submit", methods=['GET','POST'])
 def submit():
-    name = request.form['name']
-    return f"Hello {name}!"
+    if request.method=='POST':
+        name = request.form['name']
+        return f"Hello {name}!"
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
