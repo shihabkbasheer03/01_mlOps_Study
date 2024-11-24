878cb2991ec427055501276c40cf9ca9d9d2d2db
shihabkbasheer03 <shihabkbasheer03@gmail.com>
2024-11-24 15:40:38 +0530

    flask updated

--- Flask_1/app.py
+++ Flask_1/app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+
 """
-It creates an instance of the flask class, 
-Which will be WSGI application
+It creates an instance of the Flask class, 
+Which will be the WSGI application
 """
+
 ### WSGI application
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Welcome to this Flask course. This should be an amazing cource "
+    return "<html><H1>Welcome to this Flask course. This should be an amazing course </H1></html>"
 
-if __name__=="__main__":
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
     app.run(debug=True)
